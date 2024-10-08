8e2c9970efe5ec231165453e603ceb57d76c6c6e
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-08 22:08:18 +0530

    Add files via upload

---
+++ compare three value and fetch the biggest valu in js.js
@@ -0,0 +1,16 @@
+const a = 450;
+let b = 50;
+let c = 30;
+
+if ((a > b) && (a > c)) {
+    console.log(a);
+}
+else if ((b > c) && (b > c)) {
+    console.log(b);
+}
+else if ((c > a) && (c > b)) {
+    console.log(c);
+}
+
+
+
