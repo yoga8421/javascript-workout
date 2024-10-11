436590ae1e48e21f17c87190b9f822be66ce3c13
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-11 23:12:30 +0530

    Add files via upload

---
+++ let defaultArr = [5,10,15,20,25,30];.js
@@ -0,0 +1,11 @@
+let  defaultArr = [5,10,15,20,25,30];
+
+
+let inewArr=[];
+
+for(i=0;i< defaultArr.length;i++){
+   
+console.log(defaultArr)  
+    
+}
+console.log(inewArr);
---
+++ task 1.js
@@ -0,0 +1,9 @@
+// 01) Write a JavaScript program to print numbers from 1 to 10 using a for loop.
+// 	o/p --> 1,2,3,4,5,6,7,8,9,10
+
+let j=0;
+for(x=1;x<=10;x++){
+    j++;
+    console.log(j)
+}
+  
