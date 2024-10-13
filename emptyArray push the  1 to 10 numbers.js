85e34901180b990c434452b596dbf9341a4c7321
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-13 22:54:02 +0530

    Add files via upload

---
+++ emptyArray push the  1 to 10 numbers.js
@@ -0,0 +1,8 @@
+
+const emptyArray = [];
+
+for (i = 1; i <= 10; i++) {
+
+    emptyArray.push(i)
+    console.log(emptyArray);
+}
---
+++ married status print.js
@@ -0,0 +1,21 @@
+
+// isMarried using boolean
+
+let isMarried = true;
+
+
+// myAge using integer
+
+let myAge = 18;
+console.log('Age status',myAge);
+
+
+// employee detail
+let maritalStatus = true;
+let age = 20;
+let workingCompany = "LfMound";
+
+console.log('marital status',maritalStatus, 'agedetail',age, 'companyname',workingCompany);
+console.log('marriage status',isMarried);
+
+
