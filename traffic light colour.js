1d37ba2091d86389e3d7ac2fbf26b9f8f87bbbbe
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-15 23:01:55 +0530

    Add files via upload

---
+++ traffic light colour.js
@@ -0,0 +1,19 @@
+let lightColour='red';
+
+
+if(lightColour==="green"){
+    console.log("Go");
+
+}
+else if(lightColour ==="yellow"){
+    console.log("Slow");
+
+}
+else if(lightColour ==="red"){
+    console.log("stop");
+
+}
+else {
+    console.log("Invalid lightcolour");
+
+}
