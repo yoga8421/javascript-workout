14bb9dc0f5e64e67cb3bcbca73d63ace64378224
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-19 23:28:51 +0530

    Add files via upload

---
+++ Passwordlength.js
@@ -0,0 +1,16 @@
+let password ="1234567";
+
+// console.log(password.length)
+
+if(password.length<6){
+    console.log("your password is poor")
+
+}
+else if(password.length<=10){
+    console.log("your password is Medium")
+
+}
+else {
+    console.log("your password is strong")
+
+}
