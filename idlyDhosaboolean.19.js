b6afb23f9505809b46e5f611638431052e51bd08
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-21 23:52:57 +0530

    Add files via upload

---
+++ idlyDhosaboolean.19.js
@@ -0,0 +1,14 @@
+let idly = true;
+let sambar = false;
+let dhosa =true;
+let chutny =false;
+
+if((idly == true) && (sambar == true)){
+console.log("Aravind buying idlySambar")
+}
+else if((dhosa== true)&& ( chutny  == true)){
+    console.log("Aravind buying dhosaChutny")
+}
+else{
+    console.log("not available for breakfast")
+}
