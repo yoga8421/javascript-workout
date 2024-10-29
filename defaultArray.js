369c5e6d87182e52e6f52a9b276f9056e52e4bae
yogalakshmi Mariyappan <117275961+yoga8421@users.noreply.github.com>
2024-10-29 23:30:06 +0530

    Add files via upload

---
+++ defaultArray.js
@@ -0,0 +1,14 @@
+ 
+ const defaultArr = [12,67,78,45,23,567,98,27,50];
+
+ const newArr =[];
+
+ for(i=0;i<defaultArr.length;i++){
+
+if(defaultArr[i]<50){
+    newArr.push( defaultArr[i]);
+}
+
+
+ }
+ console.log(newArr);
---
+++ even number add.js
@@ -0,0 +1,9 @@
+let a = 0;
+let b = 25;
+let c = 0;
+for (i = 1; i <= b; i++) {
+    a = a + 2;
+    console.log(a);
+    c += a; 
+    console.log(c);
+}
